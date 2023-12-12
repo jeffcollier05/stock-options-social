abc2512922ebb7d4ec88bbd7519de8809bbc7591
jeffcollier05 <jeffreycollier05@gmail.com>
2023-12-11 20:36:45 -0600

    changing files of claims

--- TradeHarborApi/Common/RequestResponse.cs
+++ TradeHarborApi/Common/RequestResponse.cs
@@ -1,6 +1,6 @@
 ﻿namespace TradeHarborApi.Common
 {
-    public class Constants
+    public static class RequestResponse
     {
         public const string INVALID_AUTHENTICATION = "Invalid authentication.";
 
@@ -10,19 +10,6 @@
 
         public const string EMAIL_ALREADY_EXISTS = "The email already exists.";
 
-        public const string CLAIM_ID = "UserId";
-
         public const string USERNAME_ALREADY_EXISTS = "The username already exists.";
-    }
-
-    public class CustomClaims
-    {
-        public const string USERNAME = "Username";
-
-        public const string FIRST_NAME = "FirstName";
-
-        public const string LAST_NAME = "LastName";
-
-        public const string PROFILE_PICTURE_URL = "ProfilePictureUrl";
     }
 }
---
+++ TradeHarborApi/Common/TradeHarborClaims.cs
@@ -0,0 +1,15 @@
+﻿namespace TradeHarborApi.Common
+{
+    public static class TradeHarborClaims
+    {
+        public const string USERNAME = "Username";
+
+        public const string FIRST_NAME = "FirstName";
+
+        public const string LAST_NAME = "LastName";
+
+        public const string PROFILE_PICTURE_URL = "ProfilePictureUrl";
+
+        public const string CLAIM_ID = "UserId";
+    }
+}

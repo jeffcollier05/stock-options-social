1a83a31d0a75a92ded2cd24ebcd149ba1bdea728
jeffcollier05 <jeffreycollier05@gmail.com>
2023-12-11 22:09:01 -0600

    added model requirement

--- TradeHarborApi/Models/Friend/ModifyFriendPairRequest.cs
+++ TradeHarborApi/Models/Friend/ModifyFriendPairRequest.cs
@@ -1,7 +1,10 @@
-﻿namespace TradeHarborApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TradeHarborApi.Models
 {
     public class ModifyFriendPairRequest
     {
+        [Required(ErrorMessage = "The FriendUserId field is required.")]
         public string FriendUserId { get; set; } = string.Empty;
     }
 }
